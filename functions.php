46aa57eff19e0c0d8cd116a05e51ffbe0c3a0536
lumijoe <spacedesign63@gmail.com>
2024-08-15 02:31:03 +0900

    test

--- functions.php
+++ functions.php
@@ -1,40 +1,16 @@
 <?php
 
-/**
- * testatrail functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package testatrail
- */
-/*****************************
- * ファイルの読み込み
- ****************************/
+
+// 読み込ませたいファイルを追加
+function my_enqueue_scripts()
+{
+    wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_style('style-name', get_template_directory_uri() . '/assets/scss/pages/home.css', array(), '1.0.0', false);
+}
+add_action('wp_enqueue_scripts', 'my_enqueue_scripts');
+
 // ヘッダー・フッターのカスタムメニュー化
 register_nav_menus(array(
     'place_global' => 'グローバル',
     'place_footer' => 'フッターナビ'
 ));
-
-
-
-
-
-
-/*****************************
- * ファイルの読み込み
- ****************************/
-// 読み込ませたいファイルを追加
-function add_files()
-{
-    // メインのスタイルstyle.cssを読み込ませるget_stylesheet_uri()
-    wp_enqueue_style('main-style', get_stylesheet_uri());
-    // トップページのスタイルhome.cssを読み込ませるget_template_directory_uri() . '/assets/scss/pages/home.css', array('main-style'));
-    wp_enqueue_style('home-style', get_template_directory_uri() . "./assets/scss/pages/home.css", array('main-style'));
-    // trueがbodyのうえ、falseはheadのなかで出力される
-    wp_enqueue_script('jquery-cdn', 'https://code.jquery.com/jquery-3.6.0.min.js', array(), '3.6.0', true);
-    // js/script.jsを読み込ませる(main-scriptはjQueryの読込後に実行、footerに反映させたいtrue)
-    wp_enqueue_script('main-script', get_template_directory_uri() . '/assets/js/script.js', array('jquery-cdn'), '1.0.0', true);
-}
-// 追加したファイルを呼び出す
-add_action('wp_enqueue_scripts', 'add_files');
