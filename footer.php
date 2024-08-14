25c33b55bbce5760bc1d7cbfcfaf0d8ee803d2e3
lumijoe <spacedesign63@gmail.com>
2024-08-15 01:22:07 +0900

    test

--- footer.php
+++ footer.php
@@ -2,12 +2,15 @@
     <footer class="l-nav">
         <nav class="p-nav">
             <ul>
-                <li><a href="/#service">事業案内</a></li>
-                <li><a href="/#work">施工事例</a></li>
-                <li><a href="/#company">会社概要</a></li>
-                <li><a href="/#contact">お問い合わせ</a></li>
+                <li><a href="https://lkcodetest.sakura.ne.jp/testatrail/#service">事業案内</a></li>
+                <li><a href="https://lkcodetest.sakura.ne.jp/testatrail/#work">施工事例</a></li>
+                <li><a href="https://lkcodetest.sakura.ne.jp/testatrail/#company">会社概要</a></li>
+                <li><a href="https://lkcodetest.sakura.ne.jp/testatrail/#contact">お問い合わせ</a></li>
             </ul>
         </nav>
+        <div>
+            <img src="https://lkcodetest.sakura.ne.jp/testatrail/wp-content/uploads/2024/08/arrow_circle.png" alt="" srcset="" class=btn_totop>
+        </div>
     </footer>
     <!-- script -->
     <!-- <script src="./assets/js/script.js"></script> -->
--- front-page.php
+++ front-page.php
@@ -21,10 +21,10 @@
     <section class="l-nav">
         <nav class="p-nav">
             <ul>
-                <li><a href="/#service">事業案内</a></li>
-                <li><a href="/#work">施工事例</a></li>
-                <li><a href="/#company">会社概要</a></li>
-                <li><a href="/#contact">お問い合わせ</a></li>
+                <li><a href="https://lkcodetest.sakura.ne.jp/testatrail/#service">事業案内</a></li>
+                <li><a href="https://lkcodetest.sakura.ne.jp/testatrail/#work">施工事例</a></li>
+                <li><a href="https://lkcodetest.sakura.ne.jp/testatrail/#company">会社概要</a></li>
+                <li><a href="https://lkcodetest.sakura.ne.jp/testatrail/#contact">お問い合わせ</a></li>
             </ul>
         </nav>
     </section>
